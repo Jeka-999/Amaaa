eea5eea59f542e5daa601a102a3d855a9fb8dae6
Felix Cordonez <felix.08benaicazar@gamail.com>
2024-11-26 16:42:10 -0500

    hola

---
+++ src/main/java/com/mycompany/amaaaa/AMAAAA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.amaaaa;
+
+/**
+ *
+ * @author HP
+ */
+public class AMAAAA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
